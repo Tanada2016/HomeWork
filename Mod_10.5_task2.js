8a7265816b820b780ff92606f52ad406419c5e35
Tanada2016 <122272776+Tanada2016@users.noreply.github.com>
2023-05-20 19:48:00 +0300

    кнопка с алертом

---
+++ Mod_10.5_task2.js
@@ -0,0 +1,10 @@
+// Задание 2.
+
+// Сверстайте кнопку, клик на которую будет выводить данные о размерах экрана с помощью alert. 
+
+function showScreenSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    alert(`Размер экрана: ${width} x ${height}`);
+  }
+  
